8ce059eb354778b890ff7b1e064a7aa0c61b33dd
Adrian Dobrzański <dobrzanski.dev@gmail.com>
2022-08-13 02:02:26 +0200

    add display 100x100 mesh of randomly filled rectangles

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use coffee::graphics::{Color, Frame, Window, WindowSettings};
+use coffee::graphics::{Color, Frame, Window, WindowSettings, Mesh, Shape, Rectangle};
 use coffee::load::Task;
 use coffee::{Game, Result, Timer};
+use rand::{ Rng, thread_rng };
+
 
 fn main() -> Result<()> {
     MyGame::run(WindowSettings {
@@ -12,8 +14,29 @@ fn main() -> Result<()> {
     })
 }
 
+struct GameMatrix ([[bool; 100]; 100]);
+
+impl GameMatrix {
+    pub fn new() -> GameMatrix {
+        GameMatrix([[false; 100];100])
+    }
+
+    pub fn iter_cells(&self) -> impl Iterator<Item = ((usize, usize), &bool)> {
+        let GameMatrix(matrix) = self;
+        matrix.iter().enumerate()
+            .flat_map(|(x, col)| col.iter().enumerate().map(move |(y, cell)| ((x, y), cell)))
+    }
+}
+
+fn get_random_boolean(true_chance: f32) -> bool {
+    let mut rng = thread_rng();
+    (rng.gen_range(0.0..1.0) < true_chance) as bool
+}
+
+
 struct MyGame {
-    // Your game state and assets go here...
+   // Your game state and assets go here...
+  matrix: [[bool; 100]; 100],
 }
 
 impl Game for MyGame {
@@ -22,7 +45,16 @@ impl Game for MyGame {
 
     fn load(_window: &Window) -> Task<MyGame> {
         // Load your game assets here. Check out the `load` module!
-        Task::succeed(|| MyGame { /* ... */ })
+
+        // Init (random) state
+        let game_matrix = GameMatrix::new();
+        let GameMatrix(mut raw_matrix) = game_matrix;
+
+        for ((x, y), _) in game_matrix.iter_cells() {
+            raw_matrix[x][y] = get_random_boolean(0.3)
+        }
+
+        Task::succeed(move || MyGame { matrix: raw_matrix })
     }
 
     fn draw(&mut self, frame: &mut Frame, _timer: &Timer) {
@@ -30,5 +62,26 @@ impl Game for MyGame {
         frame.clear(Color::BLACK);
 
         // Draw your game here. Check out the `graphics` module!
+
+        // Draw state as Rectangle matrix
+        let mut mesh = Mesh::new();
+
+        for col_idx in 0..self.matrix.len() {
+            for row_idx in 0..self.matrix[col_idx].len() {
+                let cell_shape  = Shape::Rectangle(Rectangle {
+                    x: col_idx as f32 * 50.0,
+                    y: row_idx as f32 * 50.0,
+                    width: 50.0,
+                    height: 50.0,
+                });
+
+                if self.matrix[col_idx][row_idx] {
+                    mesh.fill(cell_shape, Color::WHITE);
+                }
+            }
+        }
+
+        mesh.draw(&mut frame.as_target());
     }
+
 }
